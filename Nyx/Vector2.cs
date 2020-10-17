fbd97bb8ee76d8ef5e11329e501841ad5cb5ea55
Tornike Goshadze <joffarex@gmail.com>
2020-10-17 23:48:43 +0400

    Create basic Vector2 implementation

---
+++ Nyx/Vector2.cs
@@ -0,0 +1,25 @@
+﻿namespace Nyx
+{
+    public class Vector2
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Vector2()
+        {
+            X = Zero().X;
+            Y = Zero().Y;
+        }
+
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2 Zero()
+        {
+            return new Vector2(0, 0);
+        }
+    }
+}
