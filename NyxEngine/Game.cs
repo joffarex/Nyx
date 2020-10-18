a5de0663658a7c4c0d4b624a81dd1d31d11686dc
Tornike Goshadze <joffarex@gmail.com>
2020-10-18 11:59:16 +0400

    Add simple game loop

--- NyxEngine/Game.cs
+++ NyxEngine/Game.cs
@@ -1,9 +1,29 @@
 ﻿
+using System;
+using System.Drawing;
+
 namespace NyxEngine
 {
     public class Game : NyxEngine
     {
         public Game() : base(new Vector2(690, 420), "Demo")
+        {
+        }
+
+        public override void OnLoad()
+        {
+            Console.WriteLine("Loaded...");
+            BackgroundColor = Color.BlueViolet;
+        }
+
+        private int frame = 0;
+        public override void OnUpdate()
+        {
+            Console.WriteLine($"Frames: {frame}");
+            frame++;
+        }
+
+        public override void OnDraw()
         {
         }
     }
--- NyxEngine/NyxEngine.cs
+++ NyxEngine/NyxEngine.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace NyxEngine
@@ -8,6 +10,9 @@ namespace NyxEngine
         private readonly Vector2 _screenSize = new Vector2(512, 512);
         private string _title = "New game";
         private Canvas _window;
+        private Thread _gameLoopThread;
+
+        public Color BackgroundColor = Color.Aqua;
 
         public NyxEngine()
         {
@@ -27,7 +32,42 @@ namespace NyxEngine
             _window = new Canvas();
             _window.Size = new Size((int) _screenSize.X, (int) _screenSize.Y);
             _window.Text = _title;
+            _window.Paint += Renderer;
+            
+            _gameLoopThread = new Thread(GameLoop);
+            _gameLoopThread.Start();
+            
             Application.Run(_window);
         }
+
+        private void GameLoop()
+        {
+            OnLoad();
+
+            while (_gameLoopThread.IsAlive)
+            {
+                try
+                {
+                    OnDraw();
+                    _window.BeginInvoke((MethodInvoker)delegate { _window.Refresh(); });
+                    OnUpdate();
+                    Thread.Sleep(1);
+                }
+                catch
+                {
+                    Console.WriteLine("Game is loading..");
+                }
+            }
+        }
+
+        private void Renderer(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            g.Clear(BackgroundColor);
+        }
+
+        public abstract void OnLoad();
+        public abstract void OnUpdate();
+        public abstract void OnDraw();
     }
 }
