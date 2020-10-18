013e23b6c6b1d196c700c7147388dc771f388fb0
Tornike Goshadze <joffarex@gmail.com>
2020-10-18 16:09:44 +0400

    Add level generation

--- NyxEngine/Demo/Game.cs
+++ NyxEngine/Demo/Game.cs
@@ -1,13 +1,26 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using NyxEngine.Objects;
 
 namespace NyxEngine.Demo
 {
     public class Game : NyxEngine
     {
+        public static readonly Vector2 ScreenSize = new Vector2(690, 420);
+
+        public string[,] Map =
+        {
+            {".", ".", ".", ".", "#", "#"},
+            {".", ".", ".", ".", ".", "."},
+            {".", ".", "#", ".", ".", "."},
+            {".", ".", "#", ".", "#", "."},
+            {".", "#", "#", "#", "#", "#"},
+            {".", ".", ".", "#", ".", "."}
+        };
+
         public Sprite2D Player;
 
-        public Game() : base(new Vector2(690, 420), "Demo")
+        public Game() : base(ScreenSize, "Demo")
         {
         }
 
@@ -16,7 +29,18 @@ namespace NyxEngine.Demo
             BackgroundColor = Color.Black;
 
             //Player = new Shape2D(new Vector2(10, 10), new Vector2(10, 10), Guid.NewGuid().ToString());
-            Player = new Sprite2D(new Vector2(10, 10), new Vector2(20, 20), $"{nameof(Player)}.png");
+            //Player = new Sprite2D(new Vector2(10, 10), new Vector2(20, 20), $"{nameof(Player)}.png");
+
+            var (scaleX, scaleY) =
+                new Tuple<float, float>(ScreenSize.X / Map.GetLength(0), ScreenSize.Y / Map.GetLength(1));
+            
+            for (var i = 0; i < Map.GetLength(0); i++)
+            for (var j = 0; j < Map.GetLength(1); j++)
+                if (Map[j, i] == "#")
+                {
+                    var sprite = new Sprite2D(new Vector2(i * scaleX, j * scaleY), new Vector2(scaleX, scaleY),
+                        "Block.png");
+                }
         }
 
         protected override void OnUpdate()
