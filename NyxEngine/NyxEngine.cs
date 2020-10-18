abf473f144b435798ee1796fff8c1dc95916423b
Tornike Goshadze <joffarex@gmail.com>
2020-10-18 11:39:27 +0400

    Refactor window creation logic

--- NyxEngine/NyxEngine.cs
+++ NyxEngine/NyxEngine.cs
@@ -5,19 +5,28 @@ namespace NyxEngine
 {
     public abstract class NyxEngine
     {
-        private Vector2 _screenSize = new Vector2(512,512);
+        private readonly Vector2 _screenSize = new Vector2(512, 512);
         private string _title = "New game";
-        private Canvas _window = null;
-        
+        private Canvas _window;
+
+        public NyxEngine()
+        {
+            CreateWindow();
+        }
+
         public NyxEngine(Vector2 screenSize, string title)
         {
             _screenSize = screenSize;
             _title = title;
-            
+
+            CreateWindow();
+        }
+
+        private void CreateWindow()
+        {
             _window = new Canvas();
-            _window.Size = new Size((int)this._screenSize.X, (int)this._screenSize.Y);
+            _window.Size = new Size((int) _screenSize.X, (int) _screenSize.Y);
             _window.Text = _title;
-            
             Application.Run(_window);
         }
     }
