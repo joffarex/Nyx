9f6cfde994cda02849fcebc358256321143e3b42
Tornike Goshadze <joffarex@gmail.com>
2020-10-19 23:44:48 +0400

    Setup basic GameObject and Component logic

---
+++ src/Nyx.SharpTT/Component.cs
@@ -0,0 +1,14 @@
+﻿namespace Nyx.SharpTT
+{
+    public abstract class Component
+    {
+        public GameObject GameObject { get; set; }
+
+        public void Start()
+        {
+            
+        }
+        
+        public abstract void Update(float dt); // Delta time
+    }
+}
---
+++ src/Nyx.SharpTT/GameObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Nyx.SharpTT
+{
+    public class GameObject
+    {
+        public string Name { get; }
+
+        public List<Component> Components { get; } = new List<Component>();
+
+        public GameObject(string name)
+        {
+            Name = name;
+        }
+
+        public T GetComponent<T>(T component) where T : Component
+        {
+            return Components.Any(c => component.GetType().IsAssignableFrom(c.GetType()))
+                ? component
+                : null;
+        }
+
+        public void AddComponent(Component component)
+        {
+            Components.Add(component);
+            component.GameObject = this;
+        }
+
+        public void RemoveComponent<T>(T component) where T : Component
+        {
+            if (Components.Any(c => component.GetType().IsAssignableFrom(c.GetType())))
+            {
+                Components.Remove(component);
+            }
+        }
+
+        public void Start()
+        {
+            foreach (var component in Components)
+            {
+                component.Start();
+            }
+        }
+
+        public void Update(float dt)
+        {
+            foreach (var component in Components)
+            {
+                component.Update(dt);
+            }
+        }
+    }
+}
