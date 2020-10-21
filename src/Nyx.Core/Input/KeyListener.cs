33e53c3080f0f3dfebeda17841dbdf657ebb8eb9
Tornike Goshadze <joffarex@gmail.com>
2020-10-22 01:10:21 +0400

    Create input event listeners

---
+++ src/Nyx.Core/Input/KeyListener.cs
@@ -0,0 +1,37 @@
+﻿using Silk.NET.Input.Common;
+
+namespace Nyx.Core.Input
+{
+    public class KeyListener
+    {
+        private static KeyListener _instance;
+        private static readonly object _mutex = new object();
+        private readonly IKeyboard _keyboard;
+
+        private KeyListener(IKeyboard keyboard)
+        {
+            _keyboard = keyboard;
+        }
+
+        public static KeyListener Get(IKeyboard keyboard)
+        {
+            if (_instance == null)
+            {
+                lock (_mutex)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new KeyListener(keyboard);
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public bool IsKeyPressed(Key key)
+        {
+            return _keyboard.IsKeyPressed(key);
+        }
+    }
+}
---
+++ src/Nyx.Core/Input/MouseListener.cs
@@ -0,0 +1,37 @@
+﻿using Silk.NET.Input.Common;
+
+namespace Nyx.Core.Input
+{
+    public class MouseListener
+    {
+        private static MouseListener _instance;
+        private static readonly object _mutex = new object();
+        private readonly IMouse _mouse;
+
+        private MouseListener(IMouse mouse)
+        {
+            _mouse = mouse;
+        }
+
+        public static MouseListener Get(IMouse mouse)
+        {
+            if (_instance == null)
+            {
+                lock (_mutex)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new MouseListener(mouse);
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public bool IsButtonPressed(MouseButton mouseButton)
+        {
+            return _mouse.IsButtonPressed(mouseButton);
+        }
+    }
+}
