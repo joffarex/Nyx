3c316a8d1441ef5ef5bd864baeb3a05f24c6a0bd
Tornike Goshadze <joffarex@gmail.com>
2020-10-24 00:06:55 +0400

    Extract all the rendering in scenes from main game

--- src/Nyx.Playground/Game.cs
+++ src/Nyx.Playground/Game.cs
@@ -1,43 +1,16 @@
 ﻿using System;
 using System.Drawing;
-using Nyx.Core.OpenGL;
 using Nyx.Engine;
 using Silk.NET.GLFW;
 using Silk.NET.Input.Common;
-using Silk.NET.OpenGL;
 using MouseButton = Silk.NET.Input.Common.MouseButton;
 
 namespace Nyx.Playground
 {
     public class Game : NyxEngine
     {
-        private static BufferObject<float> _vertexBufferObject;
-        private static BufferObject<uint> _elementBufferObject;
-        private static VertexArrayObject<float, uint> _vertexArrayobject;
-        private static Shader _shader;
-
-        //Vertex data, uploaded to the VBO.
-        private static readonly float[] Vertices =
-        {
-            //X    Y      Z
-            0.5f, 0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            -0.5f, 0.5f, 0.5f,
-        };
-
-        //Index data, uploaded to the EBO.
-        private static readonly uint[] Indices =
-        {
-            0, 1, 3,
-            1, 2, 3,
-        };
-
         private static Scene _currentScene;
         private static Game _instance;
-        public float b = 1;
-        public float g = 1;
-        public float r = 1;
 
         private Game()
         {
@@ -66,48 +39,18 @@ namespace Nyx.Playground
             _currentScene.Init();
         }
 
-        protected override unsafe void OnLoad()
+        protected override void OnLoad()
         {
             base.OnLoad();
 
-            // Initialize a vertex buffer that holds the vertex data
-            _vertexBufferObject = new BufferObject<float>(Gl, Vertices, BufferTargetARB.ArrayBuffer);
-
-            // Initializing a element buffer that holds the index data.
-            _elementBufferObject = new BufferObject<uint>(Gl, Indices, BufferTargetARB.ElementArrayBuffer);
-
-            // Creating a vertex array
-            _vertexArrayobject = new VertexArrayObject<float, uint>(Gl, _vertexBufferObject, _elementBufferObject);
-
-            // Create shader
-            (string vertexShaderPath, string fragmentShaderPath) =
-                GetShaderFullPaths("Shaders/shader.vert", "Shaders/shader.frag");
-            _shader = new Shader(Gl, vertexShaderPath, fragmentShaderPath);
-
-            // Tell opengl how to give the data to the shaders.
-            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), null);
-            Gl.EnableVertexAttribArray(0);
-
-            // Initial startup drawings
             ChangeScene(0);
         }
 
-        protected override unsafe void OnRender(double obj)
+        protected override void OnRender(double obj)
         {
             base.OnRender(obj);
 
-            //Bind the geometry and shader.
-            _vertexArrayobject.Bind();
-            // _shader.Use();
-
             _currentScene.Render();
-
-            //Draw the geometry.
-            Gl.DrawElements(PrimitiveType.Triangles, (uint) Indices.Length, DrawElementsType.UnsignedInt, null);
-
-            Gl.ClearColor(r, g, b, 1);
-
-            // Draw all game objects here
         }
 
 
@@ -134,13 +77,7 @@ namespace Nyx.Playground
         {
             base.OnClose();
 
-            _vertexBufferObject.Dispose();
-            _elementBufferObject.Dispose();
-            _vertexArrayobject.Dispose();
-            _shader.Dispose();
             _currentScene.Dispose();
-
-            // Clear out every object here
         }
 
         protected override void KeyDown(IKeyboard arg1, Key arg2, int arg3)
--- src/Nyx.Playground/LevelEditorScene.cs
+++ src/Nyx.Playground/LevelEditorScene.cs
@@ -1,29 +1,72 @@
 ﻿using Nyx.Core.OpenGL;
 using Nyx.Engine;
+using Silk.NET.OpenGL;
 
 namespace Nyx.Playground
 {
     public class LevelEditorScene : Scene
     {
+        private static BufferObject<float> _vertexBufferObject;
+        private static BufferObject<uint> _elementBufferObject;
+        private static VertexArrayObject<float, uint> _vertexArrayobject;
+
+        private readonly uint[] _elementArray =
+        {
+            2, 1, 0, // Top right triangle,
+            0, 1, 3, // Bottom left triangle
+        };
+
         private Shader _shader;
+
+        private readonly float[] _vertexArray =
+        {
+            // position        // color
+            0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // Bottom right 0
+            -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, // Top left 1
+            0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top right 2
+            -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, // Bottom left 3
+        };
 
         public override void Init()
         {
             string shaderPath = NyxEngine.GetFullPath("Shaders/square.glsl");
             _shader = new Shader(NyxEngine.Gl, shaderPath);
+
+            _vertexBufferObject = new BufferObject<float>(NyxEngine.Gl, _vertexArray, BufferTargetARB.ArrayBuffer);
+            _elementBufferObject =
+                new BufferObject<uint>(NyxEngine.Gl, _elementArray, BufferTargetARB.ElementArrayBuffer);
+            _vertexArrayobject =
+                new VertexArrayObject<float, uint>(NyxEngine.Gl, _vertexBufferObject, _elementBufferObject);
+
+            _vertexArrayobject.SetVertexAttribPointers3Pos4Col();
+            _vertexArrayobject.EnableVertexAttribPointers(new uint[] {0, 1});
         }
 
         public override void Update(float deltaTime)
         {
         }
 
-        public override void Render()
+        public override unsafe void Render()
         {
             _shader.Use();
+            _vertexArrayobject.Bind();
+
+            _vertexArrayobject.EnableVertexAttribPointers(new uint[] {0, 1});
+
+            NyxEngine.Gl.DrawElements(PrimitiveType.Triangles, (uint) _elementArray.Length,
+                DrawElementsType.UnsignedInt, null);
+
+            _vertexArrayobject.DisableVertexAttribPointers(new uint[] {0, 1});
+
+            NyxEngine.Gl.BindVertexArray(0);
+            NyxEngine.Gl.UseProgram(0);
         }
 
         public override void Dispose()
         {
+            _vertexBufferObject.Dispose();
+            _elementBufferObject.Dispose();
+            _vertexArrayobject.Dispose();
             _shader.Dispose();
         }
     }
