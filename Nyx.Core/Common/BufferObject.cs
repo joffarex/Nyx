c32728c92f34d0c499813d3a9531b5d6b5868634
Tornike Goshadze <joffarex@gmail.com>
2020-11-13 23:59:02 +0400

    Setup buffer and vertex array objects

---
+++ Nyx.Core/Common/BufferObject.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Nyx.Core.Common
+{
+    public class BufferObject<TDataType> : IDisposable where TDataType : unmanaged
+    {
+        public bool IsDisposed { get; private set; }
+
+        public BufferTarget BufferType { get; private set; }
+        public int Handle { get; private set; }
+
+        public unsafe BufferObject(TDataType[] data, BufferTarget bufferType, BufferUsageHint bufferUsage)
+        {
+            BufferType = bufferType;
+
+            Handle = GL.GenBuffer();
+            Bind();
+            GL.BufferData(bufferType, (IntPtr) (data.Length * sizeof(TDataType)), data, bufferUsage);
+        }
+        
+        ~BufferObject()
+        {
+            Dispose(false);
+            Console.WriteLine($"Memory leak detected on object: {this}");
+        }
+
+        public unsafe void ReBufferData(TDataType[] data)
+        {
+            GL.BufferSubData(BufferType, (IntPtr) 0, (IntPtr) (data.Length * sizeof(TDataType)), data);
+        }
+
+        public void Bind()
+        {
+            GL.BindBuffer(BufferType, Handle);
+        }
+
+        public void Dispose()
+        {
+            if (IsDisposed) return;
+
+            IsDisposed = true;
+            Dispose(true);
+            // prevent the destructor from being called
+            GC.SuppressFinalize(this);
+            // make sure the garbage collector does not eat our object before it is properly disposed
+            GC.KeepAlive(this);
+        }
+
+        public void Dispose(bool manual)
+        {
+            if (!manual) return;
+            GL.DeleteBuffer(Handle);
+        }
+    }
+}
---
+++ Nyx.Core/Common/VertexArrayObject.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK.Graphics.OpenGL4;
+
+namespace Nyx.Core.Common
+{
+    public class VertexArrayObject<TVertexType> : IDisposable
+        where TVertexType : unmanaged
+    {
+        public bool IsDisposed { get; private set; }
+
+
+        public int Handle { get; protected set; }
+
+        protected readonly List<uint> Locations = new();
+
+        public VertexArrayObject(BufferObject<TVertexType> vertexBufferObject)
+        {
+            Handle = GL.GenVertexArray();
+            Bind();
+            vertexBufferObject.Bind();
+        }
+
+        ~VertexArrayObject()
+        {
+            Dispose(false);
+            Console.WriteLine($"Memory leak detected on object: {this}");
+        }
+
+        public unsafe void VertexAttributePointer(uint location, int size, VertexAttribPointerType type,
+            uint vertexSize,
+            int offset)
+        {
+            GL.VertexAttribPointer(location, size, type, false,
+                (int) (vertexSize * sizeof(TVertexType)),
+                (IntPtr) (offset * sizeof(TVertexType)));
+            GL.EnableVertexAttribArray(location);
+            Locations.Add(location);
+        }
+
+        public void EnableVertexAttribPointers()
+        {
+            foreach (uint location in Locations)
+            {
+                GL.EnableVertexAttribArray(location);
+            }
+        }
+
+        public void Bind()
+        {
+            GL.BindVertexArray(Handle);
+        }
+
+        public void Detach()
+        {
+            GL.BindVertexArray(0);
+        }
+
+        public void DisableVertexAttribPointers()
+        {
+            foreach (uint location in Locations)
+            {
+                GL.DisableVertexAttribArray(location);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (IsDisposed) return;
+
+            IsDisposed = true;
+            Dispose(true);
+            // prevent the destructor from being called
+            GC.SuppressFinalize(this);
+            // make sure the garbage collector does not eat our object before it is properly disposed
+            GC.KeepAlive(this);
+        }
+
+        public void Dispose(bool manual)
+        {
+            if (!manual) return;
+            GL.DeleteVertexArray(Handle);
+        }
+    }
+
+
+    public class VertexArrayObject<TVertexType, TIndexType> : VertexArrayObject<TVertexType>
+        where TVertexType : unmanaged
+        where TIndexType : unmanaged
+    {
+        public VertexArrayObject(BufferObject<TVertexType> vertexBufferObject,
+            BufferObject<TIndexType> elementBufferObject) : base(vertexBufferObject)
+        {
+            elementBufferObject.Bind();
+        }
+    }
+}
