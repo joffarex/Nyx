50d1aa2eae6eb00ea25ff705046949e182740d74
Tornike Goshadze <joffarex@gmail.com>
2020-11-14 01:01:42 +0400

    Add basic asset manager

---
+++ Nyx.Core/AssetManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Nyx.Core.Common;
+using Nyx.Core.Utils;
+
+namespace Nyx.Core
+{
+    public static class AssetManager
+    {
+        private static readonly Dictionary<string, Shader> Shaders = new();
+        private static readonly Dictionary<string, Texture> Textures = new();
+
+        public static Shader GetShader(string resourceName)
+        {
+            string filePath = PathUtils.GetFullPath(resourceName);
+
+            if (Shaders.TryGetValue(filePath, out Shader shader))
+            {
+                return shader;
+            }
+
+            shader = new(filePath);
+            Shaders.Add(filePath, shader);
+            return shader;
+        }
+
+        public static T GetTexture<T>(string resourceName, Func<TextureType, string, T> factory) where T : Texture
+        {
+            string filePath = PathUtils.GetFullPath(resourceName);
+
+            if (Textures.TryGetValue(filePath, out Texture texture))
+            {
+                return (T) texture;
+            }
+
+            texture = TextureCreator.Validate<T>(TextureType.PixelSprite, filePath, factory);
+            Textures.Add(filePath, texture);
+            return (T) texture;
+        }
+    }
+}
---
+++ Nyx.Core/Utils/TextureCreator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nyx.Core.Common;
+
+namespace Nyx.Core.Utils
+{
+    public static class TextureCreator
+    {
+        public static T Validate<T>(TextureType textureType, string path, Func<TextureType, string, T> factory)
+            where T : Texture
+        {
+            return factory(textureType, path);
+        }
+    }
+}
