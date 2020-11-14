e648333f3880a31ee69916bd61de0caeef3444e5
Tornike Goshadze <joffarex@gmail.com>
2020-11-14 13:31:22 +0400

    Create basic serilog logger

---
+++ Nyx.Core/Logger/SerilogLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace Nyx.Core.Logger
+{
+    public static class SerilogLogger
+    {
+        public static readonly Serilog.Core.Logger Logger = new LoggerConfiguration().MinimumLevel.Debug().MinimumLevel
+            .Override("Microsoft", LogEventLevel.Information)
+            .Enrich.With(new ThreadEnricher())
+            .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({ThreadId}) {Message}{NewLine}{Exception}")
+            .CreateLogger();
+
+        public static ILoggerFactory Factory { get; } = new LoggerFactory().AddSerilog(Logger);
+    }
+}
